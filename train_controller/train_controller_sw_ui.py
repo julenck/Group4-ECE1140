71e45c563e0375abfac90be2a964c746cf1cd4ac
JulenCoca <julen.coca2opendeusto.es>
2025-09-30 18:10:56 -0400

    Created test ui and sw ui for train controller

--- train_controller/train_controller_sw_ui.py
+++ train_controller/train_controller_sw_ui.py
@@ -1,1 +1,35 @@
-print("hello my name is james")
+#Train Controller Software UI
+#Julen Coca-Knorr
+
+
+#import tkinter and related libraries
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox
+from tkinter import filedialog
+import json
+import os
+
+#variables for window size
+window_width = 1200
+window_height = 700
+
+class sw_train_controller_ui(tk.Tk):
+
+    def __init__(self):
+        super().__init__()#initialize the tk.Tk class
+
+
+        #set window title and size
+        self.title("Driver Interface")
+        self.geometry(str(window_width)+"x"+str(window_height))
+
+
+
+
+
+if __name__ == "__main__":
+    sw_train_controller_ui().mainloop()
+
+
+
