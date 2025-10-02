4722ae9b544f1a0780e02395f224f819e6d53a50
James Struyk <struykjames@outlook.com>
2025-10-02 14:22:24 -0400

    added hardware ui and inputs (which are GPIO code)

---
+++ train_controller/train_controller_hw_ui.py
@@ -0,0 +1,32 @@
+#Train Controller Hardware UI
+#James Struyk
+
+
+#import tkinter and related libraries
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox
+from tkinter import filedialog
+import json
+import os
+
+#variables for window size
+window_width = 1200
+window_height = 700
+
+class hw_train_controller_ui(tk.Tk):
+
+    def __init__(self):
+        super().__init__()#initialize the tk.Tk class
+
+
+        #set window title and size
+        self.title("Hardware Driver Interface")
+        self.geometry(str(window_width)+"x"+str(window_height))
+
+
+# THINGS THAT I SHOULD DISPLAY ON THE DRIVE UI FOR HARDWARE:
+# THE SPEEDOMETER, THE INFORMATION PANNEL
+
+# THINGS THAT MY HARDWARE SHOULD CONTROL ON THE DRIVER UI:
+# ALL BUTTONS, THE SPEED INPUT, THE TEMP INPUT, AND SERVICE BRAKE INPUT
