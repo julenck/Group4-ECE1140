eddb368ea8a21f1f41ce489240a3320194df10be
MariInglese <mni5@pitt.edu>
2025-10-07 01:00:02 -0400

    ctc test ui updates

--- ctc/ctc_test_ui.py
+++ ctc/ctc_test_ui.py
@@ -5,7 +5,7 @@ from tkinter import filedialog
 # Root window 
 root = tk.Tk()
 root.title("CTC Test User Interface")
-root.geometry("1200x800")
+root.geometry("1550x650")
 root.grid_rowconfigure(3,weight=1)
 root.grid_columnconfigure(0,weight=1)
 
@@ -62,6 +62,7 @@ station_box.grid(row=8,column=1,padx=5,pady=5,sticky='ew')
 arrival_time_box = tk.Entry(dispatcher_frame,font=label_font)
 arrival_time_box.grid(row=9,column=1,padx=5,pady=5,sticky='ew')
 
+
 def upload_schedule():
     file_path = filedialog.askopenfilename(
         title="Select Schedule File",
@@ -87,33 +88,81 @@ button_style = {
 }
 
 upload_button = tk.Button(dispatcher_frame,text="Upload Schedule",command=upload_schedule,font=('Times New Roman',15,'bold'),**button_style)
-upload_button.grid(row=10,column=0,columnspan=2,padx=5,pady=5,sticky='nsew',)
+upload_button.grid(row=11,column=0,columnspan=2,padx=5,pady=5,sticky='nsew',)
 
 uploaded_file_label=tk.Label(dispatcher_frame,text="",font=('Times New Roman',12,'italic'))
-uploaded_file_label.grid(row=11,column=1,padx=10,pady=10,sticky='nsew')
+uploaded_file_label.grid(row=12,column=1,padx=0,pady=0,sticky='nsew')
+
+def get_dispatcher_inputs():
+    data = {
+        "Train": train_box.get(),
+        "Line": line_box.get(),
+        "Section and Block": block_box.get(),
+        "State": state_box.get(),
+        "Suggested Speed": suggested_speed_box.get(),
+        "Authority": authority_box.get(),
+        "Direction": direction_box.get(),
+        "Station": station_box.get(),
+        "Arrival Time": arrival_time_box.get(),
+    }
+    train_input = data["Train"]
+    line_input = data["Line"]
+    section_block_input = data["Section and Block"]
+    state_input =  data["State"]
+    sugg_speed_input = data["Suggested Speed"]
+    authority_input = data["Authority"]
+    direction_input = data["Direction"]
+    station_input = data["Station"]
+    arrival_time_input = data["Arrival Time"]
+
+save_button = tk.Button(
+    dispatcher_frame,
+    text="Save",
+    command=get_dispatcher_inputs, 
+    font=('Times New Roman', 15, 'bold'),
+    bg="lightgray"
+)
+save_button.grid(row=10, column=0, columnspan=2, pady=20)
 
 # Track Model Frame 
 track_model_frame = tk.LabelFrame(inputs_frame,text='Track Model Inputs',font=label_font)
-track_model_frame.grid_columnconfigure((0,1),weight=1)
-track_model_frame.grid(row=2,column=0,sticky='nsew',padx=5,pady=5)
+track_model_frame.grid(row=1,column=3,sticky='nsew',padx=5,pady=5)
 
-tk.Label(track_model_frame, text="Station",font=label_font).grid(row=3,column=0,sticky='w',padx=5,pady=10)
-tk.Label(track_model_frame, text="Passengers Leaving Station",font=label_font).grid(row=4,column=0,sticky='w',padx=5,pady=10)
-tk.Label(track_model_frame, text="Passengers Entering Station",font=label_font).grid(row=5,column=0,sticky='w',padx=5,pady=10)
+tk.Label(track_model_frame, text="Station",font=label_font).grid(row=2,column=3,sticky='w',padx=5,pady=10)
+tk.Label(track_model_frame, text="Passengers Leaving Station",font=label_font).grid(row=3,column=3,sticky='w',padx=5,pady=10)
+tk.Label(track_model_frame, text="Passengers Entering Station",font=label_font).grid(row=4,column=3,sticky='w',padx=5,pady=10)
 
 station_box = ttk.Combobox(track_model_frame,values=["1","2","3"],font=label_font)
-station_box.grid(row=3,column=1,padx=5,pady=5,sticky='ew')
+station_box.grid(row=2,column=4,padx=5,pady=5,sticky='ew')
 
 leaving_box = tk.Entry(track_model_frame,font=label_font)
-leaving_box.grid(row=4,column=1,padx=5,pady=5,sticky='ew')
+leaving_box.grid(row=3,column=4,padx=5,pady=5,sticky='ew')
 
 entering_box = tk.Entry(track_model_frame,font=label_font)
-entering_box.grid(row=5,column=1,padx=5,pady=5,sticky='ew')
+entering_box.grid(row=4,column=4,padx=5,pady=5,sticky='ew')
 
+def get_track_model_inputs():
+    data = {
+        "Station": station_box.get(),
+        "Passengers Leaving Station": leaving_box.get(),
+        "Passengers Entering Station": entering_box.get(),
+    }
+    station_input = data["Station"]
+    leaving_input = data["Passengers Leaving Station"]
+    entering_input = data["Passengers Entering Station"]
+
+save_button_tm = tk.Button(
+    track_model_frame,
+    text="Save",
+    command=get_track_model_inputs,
+    font=('Times New Roman', 15, 'bold'),
+    bg="lightgray"
+)
+save_button_tm.grid(row=6, column=3, columnspan=2, pady=10)
+
+    
 # Track Controller Frame 
 track_controller_frame = tk.LabelFrame(inputs_frame,text='Track Controller Inputs', font=label_font)
-track_controller_frame.grid_columnconfigure((0,1),weight=1)
-track_controller_frame.grid_rowconfigure(5, weight=1)
 track_controller_frame.grid(row=1,column=1,sticky='nsew',padx=20,pady=20)
 
 tk.Label(track_controller_frame, text="Train Position",font=label_font).grid(row=2,column=0,sticky='w',padx=5,pady=10)
@@ -132,7 +181,7 @@ failure_box.grid(row=4,column=1,sticky='ew',padx=20,pady=20)
 # Lights and Gates Frame 
 lights_gates_frame = tk.LabelFrame(track_controller_frame,text='Lights and Gates',font=label_font)
 lights_gates_frame.grid_columnconfigure((0,1),weight=1)
-lights_gates_frame.grid(row=5, column=0, columnspan=2, sticky='nsew', padx=20, pady=20)
+lights_gates_frame.grid(row=5, column=0, columnspan=2, sticky='nw', padx=20, pady=20)
 
 tk.Label(lights_gates_frame,text="Section and Block",font=label_font).grid(row=6,column=0, sticky='nsew',padx=10,pady=10)
 section_block_box1 = tk.Entry(lights_gates_frame,font=label_font)
@@ -147,5 +196,33 @@ section_block_box2.grid(row=7,column=1,sticky='w',padx=10,pady=10)
 tk.Label(lights_gates_frame,text="Gate",font=label_font).grid(row=7,column=2, sticky='nsew',padx=10,pady=10)
 gate_box = ttk.Combobox(lights_gates_frame,values=["Open","Closed"],font=label_font)
 gate_box.grid(row=7,column=3,sticky='nsew',padx=10,pady=10)
+
+def get_track_controller_inputs():
+    data = {
+        "Train Position": position_box.get(),
+        "State of the Train": state_box.get(),
+        "Track Failure Mode": failure_box.get(),
+        "Section and Block1": section_block_box1.get(),
+        "Light Color": light_color_box.get(),
+        "Section and Block2": section_block_box2.get(),
+        "Gate": gate_box.get(),
+    }
+    train_position_input = data["Train Position"]
+    train_state_input = data["State of the Train"]
+    track_failure_mode_input = data["Track Failure Mode"]
+    light_loc = data["Section and Block1"]
+    light_color = data["Light Color"]
+    gate_loc = data["Section and Block2"]
+    gate_status = data["Gate"]
+
+save_button_tc = tk.Button(
+    track_controller_frame,
+    text="Save",
+    command=get_track_controller_inputs,
+    font=('Times New Roman', 15, 'bold'),
+    bg="lightgray"
+)
+save_button_tc.grid(row=6, column=0, columnspan=2, pady=20)
+
 
 root.mainloop()
