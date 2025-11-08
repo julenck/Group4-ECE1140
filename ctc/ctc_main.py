945b098148ee8eaf98523cd87d4cd9ca755d2f52
MariInglese <mni5@pitt.edu>
2025-11-08 15:09:00 -0500

    working

--- ctc/ctc_main.py
+++ ctc/ctc_main.py
@@ -3,8 +3,12 @@ from ctc_main_helper_functions import JSONFileWatcher
 from watchdog.observers import Observer
 from track.map import route_lookup_via_station, route_lookup_via_id
 
-# read from ctc_ui_inputs.json 
+# define file variables
+data_file_ctc_data = 'ctc_data.json'
 data_file_ui_inputs = 'ctc_ui_inputs.json'
+data_file_track_cont = '../ctc_track_controller.json' 
+
+# read from ctc_ui_inputs.json 
 with open(data_file_ui_inputs, "r") as f_inputs:
     inputs = json.load(f_inputs)
 
@@ -18,16 +22,31 @@ dest_id = route_lookup_via_station[station]["id"]
 print(f"dest id ={dest_id}") 
 
 # monitor ctc_track_controller.json for train position updates
-data_file_track_cont = '../ctc_track_controller.json' 
 train_pos = None
+train_state = None
 
 def track_update_handler(new_data): 
     global train_pos
+    global train_state
     try: 
+        # get data from ctc_track_controller.json
         train_pos = new_data["Trains"][train]["Train Position"]
+        train_state = new_data["Trains"][train]["Train State"]
+
         print(f"train position{train_pos}")
+
+        # update ctc_data.json
+        with open(data_file_ctc_data, "r") as f_data: 
+            data = json.load(f_data)
+        
+        data["Dispatcher"]["Trains"][train]["Position"] = train_pos
+        data["Dispatcher"]["Trains"][train]["State"] = train_state
+
+        with open(data_file_ctc_data,"w") as f_data: 
+            json.dump(data,f_data, indent=4)
     except: 
         print("Train position data missing")
+
 watcher = JSONFileWatcher(data_file_track_cont, track_update_handler)
 observer = Observer()
 observer.schedule(watcher,os.path.dirname(data_file_track_cont),recursive=False)
@@ -36,6 +55,7 @@ observer.start()
 # find all the stations we will need to stop at on the way 
 try: 
     for i in range(dest_id): 
+
         # get next station 
         test = route_lookup_via_id[i]["name"]
         print(f"next station ={test}")
@@ -48,8 +68,7 @@ try:
         next_station_loc = route_lookup_via_id[i]["block"]
         print(f"next station loc = {next_station_loc}")
 
-        # update ctc_data.json -- updates the UI 
-        data_file_ctc_data = 'ctc_data.json'
+        # open ctc_data.json -- updates the UI 
         with open(data_file_ctc_data, "r") as f_data: 
             data = json.load(f_data)
 
@@ -77,11 +96,30 @@ try:
             time.sleep(0.5)
             print("here")
 
+        # train is at station -- update ctc_data.json
+        current_station = test
+        with open(data_file_ctc_data, "r") as f_data:
+            data = json.load(f_data)
+
+        data["Dispatcher"]["Trains"][train]["Current Station"] = current_station
+
+        with open(data_file_ctc_data, "w") as f_data:
+            json.dump(data, f_data, indent=4)
+
         print(f"Train arrived at {test}")
 
         # once train gets to station, wait for dwell time 
         dwell_time_seconds = 10
         time.sleep(dwell_time_seconds)
+
+        # train is moving now, clear current station
+        with open(data_file_ctc_data, "r") as f_data:
+            data = json.load(f_data)
+
+        data["Dispatcher"]["Trains"][train]["Current Station"] = "---"  
+
+        with open(data_file_ctc_data, "w") as f_data:
+            json.dump(data, f_data, indent=4)
 
         # iterate to go to next station
         i = i+1
